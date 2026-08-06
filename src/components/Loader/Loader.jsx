7abcd594dfbfe8692bb2630d8b64e638ffaa04ac
sivaramachakradhar <sivaramisetti700@gmail.com>
2026-08-06 21:35:57 +0530

    Add Custom Loader

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,9 @@
+const Loader = () => {
+    return (
+        <div className="flex h-screen items-center justify-center">
+            <div className="h-16 w-16 animate-spin rounded-full border-4 border-slate-200 border-t-blue-600" />
+        </div>
+    );
+};
+
+export default Loader;
